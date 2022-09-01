fb72142697e6e656d27ed15c73c2da7b307ff7c9
michal <namoopsoo>
2022-09-01 12:31:08 -0400

    moar

--- compile.py
+++ compile.py
@@ -1,24 +1,30 @@
 import yaml
 
 from pathlib import Path
-
+from glom import glom
+from copy import deepcopy
+from jinja2 import Template
 
 
 def read_world():
-
-    with open('world.yml', 'r') as file
+    with open("world.yaml", "r") as file:
         world = yaml.safe_load(file)
-
+    
+    occupants = {glom(x, "occupant.name"): x for x in world["occupants"]}
     template = Path("location.template").read_text()
-    for occupant in world:
-        friend = occupant["friend"]
-        friend_location = world[]
-        data = {}
-    output = Template(template).render(
-            # occupant="HiImAnOccupant",
-            # friend="Im a Friend"
-            )
-    return output
-    # print(output)
+    for record in world["occupants"]:
+        friend = glom(record, "occupant.friend")
+        friend_location = occupants[friend]["location"]
+        data = deepcopy(record)
+        data["friend_location"] = friend_location
+        print(data)
 
-read_world()
+        output = Template(template).render(
+                data
+                # occupant="HiImAnOccupant",
+                # friend="Im a Friend"
+                )
+        break
+    #return twee
+        # print(output)
+
