cb8178ebc13e569068668892be120a8a9843219a
michal <namoopsoo>
2022-08-31 21:23:52 -0400

    first

--- compile.py
+++ compile.py
@@ -10,7 +10,9 @@ def read_world():
         world = yaml.safe_load(file)
 
     template = Path("location.template").read_text()
-    output = Template(template).render(occupant="HiImAnOccupant", friend="Im a Friend")
+    output = Template(template).render(
+            occupant="HiImAnOccupant",
+            friend="Im a Friend")
     print(output)
 
 read_world()
